4435772f6528a99f778f67b7a4e68f62647ca04f
Vooha Pabbireddy <vooha.pabbireddy@eagles.oc.edu>
2022-07-29 23:15:58 -0500

    Remove Unique participant Name

--- app/models/participants.model.js
+++ app/models/participants.model.js
@@ -2,10 +2,6 @@ module.exports = (sequelize, Sequelize) => {
     const participant = sequelize.define("participants", {
       name: {
         type: Sequelize.STRING,
-        unique: {
-          args: true,
-          msg: 'Name of the participant must be unique!',
-        }
       },
       emailId: {
         type: Sequelize.STRING
